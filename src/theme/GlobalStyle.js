cfe7dacbe2698d574c4153e4ff35a21a91e092e9
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-03-31 07:21:02 +0100

    adding colors to theme

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: 'Inter', sans-serif;
+    font-size: 62.5%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  body {
+    background-color: ${({ theme }) =>
+      theme.darkMode
+        ? theme.constants.colors.haiti
+        : theme.constants.colors.purpleGray};
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    font-family: inherit;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/theme/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    bayOfMany: 'hsl(232, 40%, 20%)',
+    bunting: 'hsl(232, 40%, 15%)',
+    burntSienna: 'hsl(360, 80%, 65%)',
+    haiti: 'hsl(232, 40%, 10%)',
+    iceCold: 'hsl(155, 50%, 95%)',
+    linkWater: 'hsl(232, 56%, 90%)',
+    mandysPink: 'hsl(0, 80%, 95%)',
+    moodyBlue: 'hsl(252, 56%, 61%)',
+    mountainMeadow: 'hsl(155, 81%, 46%)',
+    periwinkleGray: 'hsl(232, 56%, 98%)',
+    purpleGray: 'hsl(233, 56%, 95%)',
+    spindle: 'hsl(232, 57%, 85%)',
+    white: 'hsl(0, 0%, 100%)',
+  },
+  mediaQueries: {
+    desktop: 'screen and (min-width: 1024px)',
+    tablet: 'screen and (min-width: 768px)',
+  },
+};
